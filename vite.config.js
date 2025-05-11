08169281f645ceb1b94c75f296756061c6b15cdc
FelipeOBZ <felipeobz01@gmail.com>
2025-05-10 21:09:57 -0500

    feat: add CSP and HSTS headers

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     headers: {
       'Content-Security-Policy':
-        "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; object-src 'none';",
+        "default-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; object-src 'none';",
       'Strict-Transport-Security':
         'max-age=31536000; includeSubDomains; preload',
     },
