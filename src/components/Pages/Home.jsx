a298700551db29634af5bca98fd6a91902fb9267
FelipeOBZ <felipeobz01@gmail.com>
2025-05-10 20:43:44 -0500

    feat: add CSP and HSTS headers

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -6,6 +6,7 @@ export default function Home() {
       <section className="my-5">
         <article className="text-center">
           <h1 className="fw-semibold">Welcome to GitHub Finder 👋</h1>
+          <h4>CSP and HSTS Added</h4>
           <p>
             GitHub Finder is a web application that helps users retrieve
             information about their GitHub statistics, which dynamically change
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    headers: {
+      'Content-Security-Policy':
+        "default-src 'self'; script-src 'self'; object-src 'none';",
+      'Strict-Transport-Security':
+        'max-age=31536000; includeSubDomains; preload',
+    },
+  },
   plugins: [react()],
 })
