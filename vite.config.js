af54e150946e502b234cb60ac815c253bc97180c
FelipeOBZ <felipeobz01@gmail.com>
2025-05-10 21:55:29 -0500

    feat: add CSP and HSTS headers

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,23 @@ export default defineConfig({
   server: {
     headers: {
       'Content-Security-Policy':
-        "default-src 'self' 'unsafe-inline'; " +
+        "default-src 'self'; " +
         "script-src 'self' 'unsafe-inline'; " +
         "style-src 'self' 'unsafe-inline'; " +
         "img-src 'self' https://*.githubusercontent.com https://*.github.com; " +
         "connect-src 'self' https://api.github.com; " +
         "frame-src 'self' https://github.com; " +
         "navigate-to 'self' https://github.com; " +
-        "object-src 'none';",
+        "object-src 'none'; " +
+        'upgrade-insecure-requests; ' +
+        "frame-ancestors 'self';",
       'Strict-Transport-Security':
         'max-age=31536000; includeSubDomains; preload',
+      'X-Content-Type-Options': 'nosniff',
+      'X-Frame-Options': 'DENY',
+      'Referrer-Policy': 'strict-origin-when-cross-origin',
+      'Permissions-Policy':
+        'geolocation=(), microphone=(), camera=(), fullscreen=(), payment=()',
     },
   },
   plugins: [react()],
